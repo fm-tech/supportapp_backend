7c824959caf26c08ef1b6f572dcd89abb5e52fe3
fm-tech <fdmiran@gmail.com>
2022-03-17 23:18:13 -0500

    added more functionality and env variables

--- keystone.ts
+++ keystone.ts
@@ -23,15 +23,15 @@ export default withAuth(
   config({
     // the db sets the database provider - we're using sqlite for the fastest startup experience
     db: {
-      provider: 'sqlite',
-      url: 'file:./keystone.db',
+      provider: process.env.DB_PROVIDER,
+      url: process.env.DB_STRING,
     },
     graphql: {
       playground: true,
       apolloConfig: {
         plugins: [
           process.env.NODE_ENV !== 'production'
-            ? ApolloServerPluginLandingPageDisabled()
+            ? ApolloServerPluginLandingPageDisacobled()
             : ApolloServerPluginLandingPageGraphQLPlayground(),
         ],
       }
